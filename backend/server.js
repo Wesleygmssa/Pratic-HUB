0951356370a9003122e8d9ba8c7f628992ac7c30
Wesley Guerra <guerra_ssa@hotmail.com>
2024-11-15 15:19:33 -0300

    Refactor server.js to use consistent quotes and formatting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const userController = require('./controllers/userController');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const userController = require("./controllers/userController");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
@@ -9,11 +9,13 @@ const PORT = 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
+//teste commit
+
 // Rotas de CRUD para usuários
-app.post('/users', userController.createUser);
-app.get('/users', userController.getUsers);
-app.put('/users/:id', userController.updateUser);
-app.delete('/users/:id', userController.deleteUser);
+app.post("/users", userController.createUser);
+app.get("/users", userController.getUsers);
+app.put("/users/:id", userController.updateUser);
+app.delete("/users/:id", userController.deleteUser);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
